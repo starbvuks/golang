737f18b443141dacf5e5843c8938a1a3db548f22
starbvuks <starbvuks@gmail.com>
2022-08-03 15:11:55 +0530

    net package

--- go-server/main.go
+++ go-server/main.go
@@ -1,5 +1,44 @@
 package main
 
-func main() {
+import (
+	"fmt"
+	"log"
+	"net/http"
+)
 
+func funcHandler(w http.ResponseWriter, r *http.Request) {
+	if r.URL.Path != "/hello" {
+		http.Error(w, "404 not found", http.StatusNotFound)
+		return
+	}
+	if r.Method != "GET" {
+		http.Error(w, "method is not supported", http.StatusNotFound)
+	}
+	fmt.Fprintf(w, "hello")
+}
+
+func formHandler(w http.ResponseWriter, r *http.Request) {
+	if err := r.ParseForm(); err != nil {
+		fmt.Fprintf(w, "ParseForm() err: %v", err)
+		return
+	}
+	fmt.Fprintf(w, "POST req success")
+	name := r.FormValue("name")
+	address := r.FormValue("address")
+	fmt.Fprintf(w, "Name = %s\n", name)
+	fmt.Fprintf(w, "Address = %s\n", address)
+}
+
+func main() {
+	// using: FileServer(), Handle(), HandleFunc(), ListenAndServe from "net/http"
+	fileServer := http.FileServer(http.Dir("./static"))
+	http.Handle("/", fileServer)
+
+	http.HandleFunc("/form", formHandler)
+	http.HandleFunc("/hello", funcHandler)
+
+	fmt.Printf("Starting server at port 9090\n")
+	if err := http.ListenAndServe(":9090", nil); err != nil {
+		log.Fatal(err)
+	}
 }
